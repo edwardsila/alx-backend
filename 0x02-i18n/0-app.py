3b3869e3b2f26e0b501440d6c638eed0494f3a60
edwardsila <edwardsila000@gmail.com>
2024-07-31 12:34:15 +0000

    flask init

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" flask app """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
