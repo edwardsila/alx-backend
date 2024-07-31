ded5f35719c9a1b22de146d17b02393028c2785f
edwardsila <edwardsila000@gmail.com>
2024-07-31 12:41:16 +0000

    flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,8 +3,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
+def index() -> str:
     return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
