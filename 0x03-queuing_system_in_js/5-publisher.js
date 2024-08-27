90dce5100e2ec3764a1f9046a5421f8ff00b16f0
edwardsila <edwardsila000@gmail.com>
2024-08-27 14:15:01 +0000

    subscribers channels

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,31 @@
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+/**
+ * Publish a message to the 'holberton school channel' after a delay.
+ *
+ * @param {string} message - The message to publish.
+ * @param {number} time - The delay in milliseconds before publishing.
+ */
+
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send &{message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+}
+
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log('&{channel}: &{message}');
+
+  if (message === 'KILL_SERVER') {
+    client.quit();
+  }
+});
