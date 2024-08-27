5b8b91399a760b6ca87ea3a852099b79335c5fdb
edwardsila <edwardsila000@gmail.com>
2024-08-27 07:26:15 +0000

    async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,38 @@
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error('Error setting value:', err.toString());
+    } else {
+      console.log(`Reply: ${reply}`);
+    }
+  });
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.log('Error retrieving value:', err.toString());
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
